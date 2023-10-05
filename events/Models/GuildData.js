74d7450565bd8b0c2acd8b2383ec458c356be4ee
supermedman <56461191+supermedman@users.noreply.github.com>
2023-10-05 18:57:53 -0400

    Update GuildData.js

--- events/Models/GuildData.js
+++ events/Models/GuildData.js
@@ -1,8 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('GuildData', {
 
-        guildid: DataTypes.INTEGER,
-        spawnchannel: DataTypes.INTEGER,
+        guildid: DataTypes.STRING,
+        spawnchannel: DataTypes.STRING,
         guildxp: {
             type: DataTypes.INTEGER,
             defaultValue: 0,
