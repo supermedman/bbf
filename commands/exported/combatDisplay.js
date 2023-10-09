4da2d52b2861a694a81b756cd38832f9980dc180
supermedman <56461191+supermedman@users.noreply.github.com>
2023-10-09 15:25:02 -0400

    Update combatDisplay.js

--- commands/exported/combatDisplay.js
+++ commands/exported/combatDisplay.js
@@ -106,7 +106,8 @@ async function display(interaction, uData) {
                             //Steal has either been a success, or an error has occured!
                             //Generate item with actionToTake                          
                             const usedRar = actionToTake;
-                            await makeItem(enemy, interaction, user, usedRar);                           
+                            const itemRef = await makeItem(enemy, interaction, user, usedRar);
+                            await showStolen(itemRef, interaction);
                             stealDisabled = true;                         
                             await message.delete();
                             await resetHasItem(enemy, uData, interaction); //Upon completion reload enemy
@@ -162,7 +163,8 @@ async function display(interaction, uData) {
                             //Steal has either been a success, or an error has occured!
                             //Generate item with actionToTake                          
                             const usedRar = actionToTake;
-                            await makeItem(enemy, interaction, user, usedRar);
+                            const itemRef = await makeItem(enemy, interaction, user, usedRar);
+                            await showStolen(itemRef, interaction);
                             stealDisabled = true;
                             await message.delete();
                             await resetHasItem(enemy, uData, interaction); //Upon completion reload enemy
@@ -495,6 +497,26 @@ async function playerDead(user, enemy, interaction) {
         });
         collectorBut.on('end', async remove => { if (!embedMsg) { await embedMsg.delete(); } });
     });
+}
+
+//This method spawns a drop embed upon stealing an item successfully
+async function showStolen(itemRef, interaction) {
+    const item = await LootStore.findOne({ where: [{ spec_id: interaction.user.id }, { loot_id: itemRef.loot_id }] });
+
+    const iVal = (`Value: **${item.value}c**\nRarity: **${item.rarity}**\nAttack: **${item.attack}**\nType: **${item.type}**\nAmount Owned: **${item.amount}**`)
+
+    const itemDropEmbed = new EmbedBuilder()
+        .setTitle('~LOOT STOLEN~')
+        .setColor(0000)
+        .addFields({
+
+            name: (`${item.name}\n`),
+            value: iVal
+        });
+
+    await interaction.channel.send({ embeds: [itemDropEmbed] }).then(async dropEmbed => setTimeout(() => {
+        dropEmbed.delete();
+    }, 10000));
 }
 
 /**
