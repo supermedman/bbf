ec8182388ad74570c8cda108fa767f248826be26
supermedman <56461191+supermedman@users.noreply.github.com>
2023-11-01 20:34:29 -0400

    Update materialDropper.js

--- commands/exported/materialDropper.js
+++ commands/exported/materialDropper.js
@@ -136,7 +136,7 @@ async function grabMat(enemy, user, interaction) {
         console.log(successResult(`Material Dropped: ${finalMaterial.Name}`));
 
         let droppedNum = 1;
-        droppedNum += (100 * ((enemy.level * 0.01) - ((foundRar * 0.02) + 0.2)));
+        droppedNum += Math.floor(100 * ((enemy.level * 0.01) - ((foundRar * 0.02) + 0.2)));
 
         if (droppedNum <= 0) {
             droppedNum = 1;
