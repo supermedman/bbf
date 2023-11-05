c1212eb0ea40804747c4ca89f6dd3bb1b66605c5
supermedman <56461191+supermedman@users.noreply.github.com>
2023-11-05 11:40:56 -0500

    Update material.js

--- commands/material.js
+++ commands/material.js
@@ -49,10 +49,10 @@ module.exports = {
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('view')
-				.setDescription('Combine to upgrade material type and rarity!')
+				.setDescription('View all of a material type or one material type and rarity!')
 				.addStringOption(option =>
-					option.setName('type')
-						.setDescription('Material type to combine')
+					option.setName('typeview')
+						.setDescription('Material type to view')
 						.setAutocomplete(true)
 						.setRequired(true))
 				.addBooleanOption(option =>
@@ -61,7 +61,7 @@ module.exports = {
 						.setRequired(true))
 				.addStringOption(option =>
 					option.setName('rarity')
-						.setDescription('Material rarity to combine')
+						.setDescription('Material rarity to view')
 						.setAutocomplete(true))),
 	async autocomplete(interaction) {
 		const focusedOption = interaction.options.getFocused(true);
@@ -83,6 +83,23 @@ module.exports = {
 					filtered.map(choice => ({ name: choice, value: choice })),
 				);
 			}	
+		}
+
+		if (focusedOption.name === 'typeview') {
+			const focusedValue = interaction.options.getFocused(false);
+
+			choices = ["slimy", "rocky", "woody", "skinny", "herby", "gemy", "magical", "metalic", "fleshy", "unique"];
+
+			if (focusedValue) {
+				console.log(choices);
+				console.log(focusedValue);
+
+				//Mapping the complete list of options for discord to handle and present to the user
+				const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+				await interaction.respond(
+					filtered.map(choice => ({ name: choice, value: choice })),
+				);
+			}
 		}
 
 		if (focusedOption.name === 'rarity') {
@@ -308,30 +325,33 @@ module.exports = {
 			} else if (showAll === false) {
 				if (!rarType) return interaction.followUp('Please select a rarity to display!');
 				var chosenRarID;
-				if (rarType === 'common') {
-					chosenRarID = 0;
-				} else if (rarType === 'uncommon') {
-					chosenRarID = 1;
-				} else if (rarType === 'rare') {
-					chosenRarID = 2;
-				} else if (rarType === 'very rare') {
-					chosenRarID = 3;
-				} else if (rarType === 'epic') {
-					chosenRarID = 4;
-				} else if (rarType === 'mystic') {
-					chosenRarID = 5;
-				} else if (rarType === '?') {
-					chosenRarID = 6;
-				} else if (rarType === '??') {
-					chosenRarID = 7;
-				} else if (rarType === '???') {
-					chosenRarID = 8;
-				} else if (rarType === '????') {
-					chosenRarID = 9;
+				if (matType === 'unique') {
+					return await interaction.channel.send('You cannot view one of unique material type, please use this command where <all> is <true> and leave the <rarity> option blank!');
 				} else {
-					return interaction.followUp('That was not a valid option!');
-				}
-
+					if (rarType === 'common') {
+						chosenRarID = 0;
+					} else if (rarType === 'uncommon') {
+						chosenRarID = 1;
+					} else if (rarType === 'rare') {
+						chosenRarID = 2;
+					} else if (rarType === 'very rare') {
+						chosenRarID = 3;
+					} else if (rarType === 'epic') {
+						chosenRarID = 4;
+					} else if (rarType === 'mystic') {
+						chosenRarID = 5;
+					} else if (rarType === '?') {
+						chosenRarID = 6;
+					} else if (rarType === '??') {
+						chosenRarID = 7;
+					} else if (rarType === '???') {
+						chosenRarID = 8;
+					} else if (rarType === '????') {
+						chosenRarID = 9;
+					} else {
+						return interaction.followUp('That was not a valid option!');
+					}
+                }
 				const fullMatMatchList = await MaterialStore.findAll({ where: [{ spec_id: interaction.user.id }, { rar_id: chosenRarID }, { mattype: matType }] });
 
 				if (fullMatMatchList.length <= 0) return interaction.followUp('You have no materials of that type or rarity!');
