20818f40d85e58ba1662f39cc62428fecc04595c
supermedman <56461191+supermedman@users.noreply.github.com>
2023-11-09 16:15:51 -0500

    Update sell.js

--- commands/sell.js
+++ commands/sell.js
@@ -27,36 +27,61 @@ module.exports = {
                 .addStringOption(option =>
                     option.setName('rarity')
                         .setDescription('The rarity to be sold')
+                        .setAutocomplete(true)
                         .setRequired(true))),
 	async autocomplete(interaction) {
 		//Focused option is assigned to what the user is inputting as the paramaters for what option to select from
-		const focusedValue = interaction.options.getFocused();
+		const focusedOption = interaction.options.getFocused(true);
 		//FIGURE OUT HOW TO MAP CURRENT INVENTORY ITEMS AS THE OPTIONS FOR SELECTING
-		const items = await LootStore.findAll({ where: [{ spec_id: interaction.user.id }] });
+		
 
         let choices = [];
 
-        if (focusedValue) {
-            let first = focusedValue.charAt();
+        if (focusedOption.name === 'item') {
+            const focusedValue = interaction.options.getFocused(false);
 
-            for (var n = 0; n < items.length; n++) {
-                if (items[n].name.charAt() === first) {//Check for item starting with the letter provided
-                    var picked = items[n].name;//assign picked to item name at postion n in the items list found
-                    //prevent any type errors			
-                    choices.push(picked.toString());//push each name one by one into the choices array
-                } else {
-                    //Item name does not match keep looking
+            const items = await LootStore.findAll({ where: [{ spec_id: interaction.user.id }] });
+
+            if (focusedValue) {
+                let first = focusedValue.charAt();
+
+                for (var n = 0; n < items.length; n++) {
+                    if (items[n].name.charAt() === first) {//Check for item starting with the letter provided
+                        var picked = items[n].name;//assign picked to item name at postion n in the items list found
+                        //prevent any type errors			
+                        choices.push(picked.toString());//push each name one by one into the choices array
+                    } else {
+                        //Item name does not match keep looking
+                    }
+
                 }
+                console.log(choices);
+                console.log(focusedValue);
 
+                //Mapping the complete list of options for discord to handle and present to the user
+                const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+                await interaction.respond(
+                    filtered.map(choice => ({ name: choice, value: choice })),
+                );
             }
-            console.log(choices);
-            console.log(focusedValue);
+        }
+        
 
-            //Mapping the complete list of options for discord to handle and present to the user
-            const filtered = choices.filter(choice => choice.startsWith(focusedValue));
-            await interaction.respond(
-                filtered.map(choice => ({ name: choice, value: choice })),
-            );
+        if (focusedOption.name === 'rarity') {
+            const focusedValue = interaction.options.getFocused(false);
+
+            choices = ["common", "uncommon", "rare", "very rare", "epic", "mystic", "?", "??", "???", "????"];
+
+            if (focusedValue) {
+                console.log(choices);
+                console.log(focusedValue);
+
+                //Mapping the complete list of options for discord to handle and present to the user
+                const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+                await interaction.respond(
+                    filtered.map(choice => ({ name: choice, value: choice })),
+                );
+            }
         }
 	},
     async execute(interaction) {
@@ -287,9 +312,7 @@ module.exports = {
         }
 
         if (interaction.options.getSubcommand() === 'all') {
-            const rawRaritySelected = interaction.options.getString('rarity');
-            console.log(`The rarity selected: ${rawRaritySelected}`);
-            const rarityUsed = rawRaritySelected.toLowerCase();
+            const rarityUsed = interaction.options.getString('rarity');
 
             var chosenRarID;
 
@@ -696,7 +719,19 @@ module.exports = {
                     console.log('COINS UPDATED!');
                     console.log('Item sold! You gained: ', soldFor);
 
-                    await interaction.followUp(`Item sold! You gained: ${soldFor}c`);
+                    const soldEmbed = {
+                        title: 'Item Sold!',
+                        color: 0000,
+                        fields: [{
+                            name: 'You gained: ', value: `${soldFor}c`,
+                        }],
+                    };
+
+                    await interaction.followUp({ embeds: [soldEmbed] }).then(async embedMsg => setTimeout(() => {
+                        embedMsg.delete();
+                    }, 1000));
+
+                    //await interaction.followUp(`Item sold! You gained: ${soldFor}c`);
                     if (newA <= 0) {
                         //no more items remove from table
                         const newtot = uData.totitem - 1;
@@ -737,7 +772,17 @@ module.exports = {
                     console.log('COINS UPDATED!');
                     console.log('Item sold! You gained: ', soldFor);
 
-                    await interaction.followUp(`Item sold! You gained: ${soldFor}c`);
+                    const soldEmbed = {
+                        title: 'Item Sold!',
+                        color: 0000,
+                        fields: [{
+                            name: 'You gained: ', value: `${soldFor}c`,
+                        }],
+                    };
+
+                    await interaction.followUp({ embeds: [soldEmbed] }).then(async embedMsg => setTimeout(() => {
+                        embedMsg.delete();
+                    }, 1000));
                     return;
                 }
             } else { }//something went wrong                  
@@ -762,7 +807,17 @@ module.exports = {
                     console.log('COINS UPDATED!');
                     console.log('Item sold! You gained: ', soldFor);
 
-                    await interaction.followUp(`Item sold! You gained: ${soldFor}c`);
+                    const soldEmbed = {
+                        title: 'Item Sold!',
+                        color: 0000,
+                        fields: [{
+                            name: 'You gained: ', value: `${soldFor}c`,
+                        }],
+                    };
+
+                    await interaction.followUp({ embeds: [soldEmbed] }).then(async embedMsg => setTimeout(() => {
+                        embedMsg.delete();
+                    }, 1000));
                     if (newA <= 0) {
                         //no more items remove from table
                         const newtot = uData.totitem - 1;
@@ -795,7 +850,17 @@ module.exports = {
                     console.log('COINS UPDATED!');
                     console.log('Item sold! You gained: ', item.value);
 
-                    await interaction.followUp(`Item sold! You gained: ${item.value}c`);
+                    const soldEmbed = {
+                        title: 'Item Sold!',
+                        color: 0000,
+                        fields: [{
+                            name: 'You gained: ', value: `${item.value}c`,
+                        }],
+                    };
+
+                    await interaction.followUp({ embeds: [soldEmbed] }).then(async embedMsg => setTimeout(() => {
+                        embedMsg.delete();
+                    }, 1000));
 
                     if (newA <= 0) {
                         //no more items remove from table
