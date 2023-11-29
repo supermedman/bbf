0c48738c6b4d478c0bd34b066c449008b6034ba8
supermedman <56461191+supermedman@users.noreply.github.com>
2023-11-29 13:32:43 -0500

    Update addpoint.js

--- commands/addpoint.js
+++ commands/addpoint.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { errorForm } = require('../chalkPresets.js');
 const { UserData } = require('../dbObjects.js');
 
 module.exports = {
@@ -23,89 +24,90 @@ module.exports = {
 		);
 	},
 	async execute(interaction) {
-		await interaction.deferReply();
+		await interaction.deferReply().then(async () => {
+			const skillname = interaction.options.getString('skill');
+			const pointcount = interaction.options.getInteger('amount');
 
-		const skillname = interaction.options.getString('skill');
-		const pointcount = interaction.options.getInteger('amount');
+			uData = await UserData.findOne({ where: { userid: interaction.user.id } });
+			if (uData) {
+				if (uData.points > 0) {
+					if (uData.points >= pointcount) {
+						//SPEED========================
+						if (skillname === 'speed') {
+							const spd = uData.speed += pointcount;
+							const rem = uData.points = uData.points - pointcount;
 
-		uData = await UserData.findOne({ where: { userid: interaction.user.id } });
-		if (uData) {
-			if (uData.points > 0) {
-				if (uData.points >= pointcount) {
-					//SPEED========================
-					if (skillname === 'speed') {
-						const spd = uData.speed += pointcount;
-						const rem = uData.points = uData.points - pointcount;
+							const incStat = await UserData.update({ speed: spd }, { where: { username: uData.username } });
+							const removePoint = await UserData.update({ points: rem }, { where: { username: uData.username } });
+							console.log(removePoint);
 
-						const incStat = await UserData.update({ speed: spd }, { where: { username: uData.username } });
-						const removePoint = await UserData.update({ points: rem }, { where: { username: uData.username } });
-						console.log(removePoint);
-
-						if (incStat > 0) {
-							return interaction.followUp(`Speed stat has been increased for ${uData.username} succsesfully!`);
+							if (incStat > 0) {
+								return interaction.followUp(`Speed stat has been increased for ${uData.username} succsesfully!`);
+							}
+							return interaction.followUp(`Could not find data for ${uData.username}.`);
 						}
-						return interaction.followUp(`Could not find data for ${uData.username}.`);						
-					}
-					//STRENGTH========================
-					if (skillname === 'strength') {
-						const str = uData.strength += pointcount;
-						const rem = uData.points = uData.points - pointcount;
-						var tothp = 100 + (str * 10);
-						if (uData.health != (tothp - 10)) {
-							//player is currently wounded add 10 to current health
-							tothp = (uData.health + 10);
-                        }
-						
-						const incStat = await UserData.update({ strength: str }, { where: { username: uData.username } });
-						const incHealth = await UserData.update({ health: tothp }, { where: { username: uData.username } });
-						const removePoint = await UserData.update({ points: rem }, { where: { username: uData.username } });
-						console.log(removePoint);
+						//STRENGTH========================
+						if (skillname === 'strength') {
+							const str = uData.strength += pointcount;
+							const rem = uData.points = uData.points - pointcount;
+							var tothp = 100 + (str * 10);
+							if (uData.health != (tothp - 10)) {
+								//player is currently wounded add 10 to current health
+								tothp = (uData.health + 10);
+							}
 
-						if (incStat > 0) {
-							if (incHealth > 0) {
-								return interaction.followUp(`Strength stat has been increased for ${uData.username} succsesfully!`);
-                            }
+							const incStat = await UserData.update({ strength: str }, { where: { username: uData.username } });
+							const incHealth = await UserData.update({ health: tothp }, { where: { username: uData.username } });
+							const removePoint = await UserData.update({ points: rem }, { where: { username: uData.username } });
+							console.log(removePoint);
+
+							if (incStat > 0) {
+								if (incHealth > 0) {
+									return interaction.followUp(`Strength stat has been increased for ${uData.username} succsesfully!`);
+								}
+							}
+							return interaction.followUp(`Could not find data for ${uData.username}.`);
 						}
-						return interaction.followUp(`Could not find data for ${uData.username}.`);					
-					}
-					//dexterity========================
-					if (skillname === 'dexterity') {
-						const dex = uData.dexterity += pointcount;
-						const rem = uData.points = uData.points - pointcount;
+						//dexterity========================
+						if (skillname === 'dexterity') {
+							const dex = uData.dexterity += pointcount;
+							const rem = uData.points = uData.points - pointcount;
 
-						const incStat = await UserData.update({ dexterity: dex }, { where: { username: uData.username } });
-						const removePoint = await UserData.update({ points: rem }, { where: { username: uData.username } });
-						console.log(removePoint);
+							const incStat = await UserData.update({ dexterity: dex }, { where: { username: uData.username } });
+							const removePoint = await UserData.update({ points: rem }, { where: { username: uData.username } });
+							console.log(removePoint);
 
-						if (incStat > 0) {
-							return interaction.followUp(`Dexterity stat has been increased for ${uData.username} succsesfully!`);
+							if (incStat > 0) {
+								return interaction.followUp(`Dexterity stat has been increased for ${uData.username} succsesfully!`);
+							}
+							return interaction.followUp(`Could not find data for ${uData.username}.`);
 						}
-						return interaction.followUp(`Could not find data for ${uData.username}.`);					
-					}
-					//intelligence========================
-					if (skillname === 'intelligence') {
-						const int = uData.intelligence += pointcount;
-						const rem = uData.points = uData.points - pointcount;
+						//intelligence========================
+						if (skillname === 'intelligence') {
+							const int = uData.intelligence += pointcount;
+							const rem = uData.points = uData.points - pointcount;
 
-						const incStat = await UserData.update({ intelligence: int }, { where: { username: uData.username } });
-						const removePoint = await UserData.update({ points: rem }, { where: { username: uData.username } });
-						console.log(removePoint);
+							const incStat = await UserData.update({ intelligence: int }, { where: { username: uData.username } });
+							const removePoint = await UserData.update({ points: rem }, { where: { username: uData.username } });
+							console.log(removePoint);
 
-						if (incStat > 0) {
-							return interaction.followUp(`Intelligence stat has been increased for ${uData.username} succsesfully!`);
+							if (incStat > 0) {
+								return interaction.followUp(`Intelligence stat has been increased for ${uData.username} succsesfully!`);
+							}
+							return interaction.followUp(`Could not find data for ${uData.username}.`);
 						}
-						return interaction.followUp(`Could not find data for ${uData.username}.`);					
+						else {
+							return interaction.channel.send(`You have not selected a valid skill to increase, ${interaction.username}!`);
+						}
 					}
-					else {
-						return interaction.channel.send(`You have not selected a valid skill to increase, ${interaction.username}!`);
-					}
+					return interaction.followUp('You do not have enough points, to check your current point total please use ``/stats user``');
+				} else if (uData.points <= 0) {
+					return interaction.followUp("You have no perk points.. kill enemies for xp to level up, each time you level up you will gain a point!");
 				}
-				return interaction.followUp('You do not have enough points, to check your current point total please use ``/stats user``');
-			} else if (uData.points <= 0) {
-				return interaction.followUp("You have no perk points.. kill enemies for xp to level up, each time you level up you will gain a point!");
 			}
-		}
-		
+		}).catch(error => {
+			console.log(errorForm(error));
+		});
 	},
 
 };
