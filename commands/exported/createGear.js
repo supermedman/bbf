42c74975264509fdcf2a19d17a9771b18aa7bd30
supermedman <56461191+supermedman@users.noreply.github.com>
2023-12-12 21:50:54 -0500

    Added createGear.js

---
+++ commands/exported/createGear.js
@@ -0,0 +1,74 @@
+const { LootStore } = require('../../dbObjects.js');
+
+
+async function checkOwned(user, item) {
+    const lootStore = await LootStore.findOne({
+        where: { spec_id: user.userid, loot_id: item.Loot_id },
+    });
+
+    let tableUpdated = '';
+    if (lootStore) {
+        tableUpdated = await increaseItem(lootStore);
+    } else tableUpdated = await makeItem(user, item);
+
+    if (tableUpdated !== 'Success') return `Error: ${tableUpdated}`;
+    return 'Finished';
+}
+
+async function increaseItem(tableRef) {
+    const inc = await tableRef.increment('amount');
+
+    if (inc) {
+        await tableRef.save();
+        return 'Success';
+    } else return 'Item Count Increase Failure: CODE 1';
+}
+
+async function makeItem(user, item) {
+    // Increase total item count by 1 and save
+    user.totitem += 1;
+    await user.save();
+
+    // Check item slot for assignments
+    let slotCheck = item.Slot;
+
+    // Dynamic value placeholders
+    let dynHands = 'NONE';
+    let dynAtk = 0;
+    let dynDef = 0;
+    if (slotCheck === 'Mainhand') {
+        // If item is mainhand only hands and attack are needed
+        dynHands = item.Hands;
+        dynAtk = item.Attack;
+    } else if (slotCheck === 'Offhand') {
+        // If item is offhand hands is One, both attack and defence are needed
+        dynHands = 'One';
+        dynAtk = item.Attack;
+        dynDef = item.Defence;
+    } else {
+        // Else item is armor and only defence is needed
+        dynDef = item.Defence;
+    }
+
+    // Add new item with values filtered through 
+    const isDone = await LootStore.create({
+        name: item.Name,
+        value: item.Value,
+        loot_id: item.Loot_id,
+        spec_id: user.userid,
+        rarity: item.Rarity,
+        rar_id: item.Rar_id,
+        attack: dynAtk,
+        defence: dynDef,
+        type: item.Type,
+        slot: item.Slot,
+        hands: dynHands,
+        amount: 1
+    });
+
+    if (isDone) {
+        return 'Success';
+    } else return 'Item Create Failure: CODE 2';
+}
+
+module.exports = { checkOwned, increaseItem, makeItem };
