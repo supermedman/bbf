88d2c2cf29873ed84d2950ca9853343606c3dadb
Th3Ward3n <liam.hatton@gmail.com>
2024-02-23 22:43:16 -0500

    Updated Cheats

--- commands/Development/cheat.js
+++ commands/Development/cheat.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 const {UserData} = require('../../dbObjects.js');
 const lootList = require('../../events/Models/json_prefabs/lootList.json');
+const { checkOwned } = require('../Game/exported/createGear.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,7 +10,10 @@ module.exports = {
         .setDescription('Dev Command')
         .addIntegerOption(option =>
             option.setName('id')
-                .setDescription('Item ID')),
+                .setDescription('Item ID'))
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('Item Amount')),
 
 	async execute(interaction) { 
         if (interaction.user.id !== '501177494137995264') return await interaction.reply('Nope!');
@@ -18,5 +22,11 @@ module.exports = {
 
         let filterItem = lootList.filter(item => item.Loot_id === itemID);
         
+        filterItem = filterItem[0];
+        
+        const user = await UserData.findOne({where: {userid: interaction.user.id}});
+
+        const outcome = await checkOwned(user, filterItem, 1);
+        if (outcome === 'Finished') return await interaction.reply('Item added!', filterItem.Name);
     },
 };
