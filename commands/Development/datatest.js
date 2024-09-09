654022f5ab4d0a5cf135daf4f282e1fbfb0ea149
Th3Ward3n <liam.hatton@gmail.com>
2024-09-08 21:22:22 -0400

    Further material dev testing

--- commands/Development/datatest.js
+++ commands/Development/datatest.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 const {chlkPreset} = require('../../chalkPresets.js');
 const chalk = require('chalk');
-const { handleLimitOnOptions } = require('../../uniHelperFunctions.js');
-const { checkingRarID, loadFullRarNameList } = require('./Export/itemStringCore.js');
-const { UserMaterials } = require('../../dbObjects.js');
+const { handleLimitOnOptions, endTimer, randArrPos } = require('../../uniHelperFunctions.js');
+const { checkingRarID, loadFullRarNameList, loadFullDismantleList } = require('./Export/itemStringCore.js');
+const { UserMaterials, MaterialStore } = require('../../dbObjects.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -91,7 +91,7 @@ module.exports = {
             const focusedValue = interaction.options.getFocused(false);
 
             // Load Choices
-            choices = ['MAT-load-store', 'MAT-clear-store', 'MAT-rand-load-static', 'MAT-rand-load-store'];
+            choices = ['MAT-load-store', 'MAT-clear-store', 'MAT-update-store', 'MAT-rand-load-static', 'MAT-rand-load-store'];
 
             const filtered = choices.filter(choice => choice.startsWith(focusedValue));
             await interaction.respond(
@@ -106,7 +106,6 @@ module.exports = {
         const subCom = interaction.options.getSubcommand();
 
         const startTime = new Date().getTime();
-        let endTime;
 
         // TO DO
         // ===========================
@@ -140,8 +139,8 @@ module.exports = {
         };
 
         const isUnique = r => convertRarToID(r) === 12;
-        const isUniqueMatch = (mat, u) => mat.UniqueMatch === u;
-        const isMatMatch = (mat, r) => mat.Rar_id === convertRarToID(r);
+        const isUniqueRefMatch = (mat, u) => mat.UniqueMatch === u;
+        const isMatRefMatch = (mat, r) => mat.Rar_id === convertRarToID(r);
 
         /**
          * This function checks if the given params require checking for a `unique` material,
@@ -162,12 +161,12 @@ module.exports = {
          */
         const isRefMatch = (r, mat, u) => {
             if (u && isUnique(r)){
-                return isUniqueMatch(mat, u);
-            } else return isMatMatch(mat, r);
+                return isUniqueRefMatch(mat, u);
+            } else return isMatRefMatch(mat, r);
         };
 
         /** 
-         * @type {{type: string, options: (number[] | string[])}[]}
+         * @type {{type: string, options: (number | string)[]}[]}
          */
         const rarLoadTypes = [
             {
@@ -176,11 +175,11 @@ module.exports = {
             },
             {
                 type: "ID",
-                options: Array.from(new Array(19).fill(0), (v, t) => t + v)
+                options: Array.from(new Array(11).fill(0), (v, t) => t + v)
             },
             {
                 type: "sID",
-                options: Array.from(new Array(19).fill(0), (v, t) => `${t + v}`)
+                options: Array.from(new Array(11).fill(0), (v, t) => `${t + v}`)
             }
         ];
         // ==========================
@@ -205,7 +204,7 @@ module.exports = {
             case "combat":
                 switch(subCom){
                     case "simulation":
-                        
+                        // NPC testing
                     break;
                     case "full":
                         
@@ -222,7 +221,7 @@ module.exports = {
             case "crafting":
                 switch(subCom){
                     case "create":
-                        
+                        // NPC testing
                     break;
                 }
             break;
@@ -233,15 +232,17 @@ module.exports = {
                         const simCAT = simType.split('-')[0];
                         if (simCAT === 'MAT'){
                             // Material based tests
-                            
-                            
-                            // ['MAT-load-store', 'MAT-clear-store', 'MAT-rand-load-static', 'MAT-rand-load-store']
+                            // ['MAT-load-store', 'MAT-clear-store', 'MAT-update-store', 'MAT-rand-load-static', 'MAT-rand-load-store']
                             if (simType === 'MAT-load-store'){
                                 await generateMaterialStores(interaction);
                             }
 
                             if (simType === 'MAT-clear-store'){
 
+                            }
+
+                            if (simType === 'MAT-update-store'){
+                                await updateMatchingMaterialStore(interaction);
                             }
 
                             if (simType === 'MAT-rand-load-static'){
@@ -256,24 +257,30 @@ module.exports = {
                 }
             break;
         }           
-        endTime = new Date().getTime();
+        endTimer(startTime, `Data Test ${subComGroup} ${subCom}`); 
 
+        const endTime = new Date().getTime();
         return await interaction.reply(`Command took ${endTime - startTime}ms to complete!`);
 
         // Material Related Functions
+        /**
+         * This function attempts to retireve the contents of `interaction.client.materials.get(mType)[mUType ?? mRar]` where
+         * `mType`, `mUType`, and `mRar` are chosen at random.
+         * @param {object} interaction 
+         */
         function retrieveStaticMatRef(interaction){
             const {materials} = interaction.client;
         
             // Any method of obtaining mattype as string
-            const mType = randArrPos(loadFullDismantleList()); // 'slimy'; 
+            const mType = randArrPos(loadFullDismantleList([])); // 'slimy'; 
         
             // randArrPos([{type: "string"}, {type: "number"}, {type: "string number id"}])
             
             // Any method of obtaining rarity/rar_id either string or number
-            const mRar = randArrPos((randArrPos(rarLoadTypes)).options);
+            const mRar = (mType !== 'unique') ? randArrPos((randArrPos(rarLoadTypes)).options) : randArrPos(['unique', 12, "12"]);
         
             // If material is unique type string, otherwise undefined
-            const mUType = undefined; // 'Dark' || undefined
+            const mUType = (mType !== 'unique') ? undefined : randArrPos((materials.get(mType)).map(mat => mat.UniqueMatch)); // 'Dark' || undefined
         
             // Grab cached material type list
             const matDataList = materials.get(mType);
@@ -284,16 +291,24 @@ module.exports = {
             console.log(`Random Details:\nMatType: ${mType}\nMatRarity: ${mRar}\nMat Match Found: `, matMatch);
         }
         
-        
+        /**
+         * This function attempts to retireve the contents of `UserMaterials[type].matdata[uT | mR]` where 
+         * `type`, `uT`, and `mR` are randomly chosen.
+         * @param {object} interaction Base Interaction Object
+         */
         async function retrieveStoredMatData(interaction){
+            const {materials} = interaction.client;
             // Retrieve ALL Single User Stored materials
             // const userMats = await UserMaterials.findAll({where: {userid: user.userid}});
         
             // Defined by some interaction
             // Any method of obtaining mattype as string
-            const mType = randArrPos(loadFullDismantleList()); 
+            const mType = randArrPos(loadFullDismantleList([])); 
             // Any method of obtaining rarity/rar_id either string or number
-            const mRar = randArrPos((randArrPos(rarLoadTypes)).options);
+            const mRar = (mType !== 'unique') ? randArrPos((randArrPos(rarLoadTypes)).options) : randArrPos(['unique', 12, "12"]);
+
+            // If material is unique type string, otherwise undefined
+            const mUType = (mType !== 'unique') ? undefined : randArrPos((materials.get(mType)).map(mat => mat.UniqueMatch));
         
             // Retrieve ONE Single User Stored Material Type
             const userMatMatch = await UserMaterials.findOne({where: {userid: interaction.user.id, mattype: mType}});
@@ -302,12 +317,122 @@ module.exports = {
             const mdObj = JSON.parse(userMatMatch.matdata);
         
             // typeof `storedMatData` === 'number' && `storedMatData` === Material Amount Owned 
-            const storedMatData = mdObj[`${(convertRarToID(mRar)).toString()}`];
+            const matPropVal = (!mUType) ? (convertRarToID(mRar)).toString() : mUType;
+            const storedMatData = mdObj[`${matPropVal}`];
         
-            console.log(`Contents of UserMaterials where:\nMatType: ${mType}\nMatRarity: ${mRar}\nContents: `, storedMatData);
+            console.log(`Contents of UserMaterials where:\nMatType: ${mUType ?? mType}\nMatRarity: ${mRar}\nContents: `, storedMatData);
+        }
+
+        /**
+         * This function attempts to updated every `UserMaterials` entry associated with the given user.
+         * For each table entry found, compares stored user materials, updating any matching all values checked for.
+         * Saves and reloads all updated tables before returning.
+         * @param {object} interaction Base Interaction Object
+         */
+        async function updateMatchingMaterialStore(interaction){
+            const {materials} = interaction.client;
+
+            /**
+             * This function provides faster script writing
+             * @param {string} t MatType 
+             * @returns {Promise <object>}
+             */
+            const grabMatTypeEntry = async t => await UserMaterials.findOne({where: {userid: interaction.user.id, mattype: t}});
+
+            /**
+             * This function updates the `UserMaterials` Table entry matching the using user and the given `t`
+             * @template U
+             * @param {string} t MatType
+             * @param {U} upObj Updated Value Object constructed based on the owned materials/amounts of the user
+             */
+            const updateMatTypeEntry = async (t, upObj) => {
+                const UMTE = await grabMatTypeEntry(t);
+                await UMTE.update({matdata: JSON.stringify(upObj)})
+                .then(async umte => await umte.save())
+                .then(async umte => {return await umte.reload()});
+                return UMTE;
+            };
+
+            /**
+             * This function handles loading both the `searchFor` and `mats` arrays depending on the `t` given
+             * 
+             * Returns `found: false` if no mats pass first DB Query
+             * @param {string} t MatType being checked
+             * @returns {Promise <{found: boolean, mats: object[], searchFor: (number | string)[]}>}
+             */
+            const grabStoredMaterials = async t => {
+                const returnObj = {found: false, mats: [], searchFor: []};
+                if (t === 'unique'){
+                    returnObj.mats = await MaterialStore.findAll({where: {spec_id: interaction.user.id, rar_id: 12}});
+                    if (!returnObj.mats.length) return returnObj;
+                    returnObj.searchFor = returnObj.mats.map(m => m.mattype);
+                    returnObj.found = true;
+                    return returnObj;
+                }
+
+                returnObj.mats = await MaterialStore.findAll({where: {spec_id: interaction.user.id, mattype: t}});
+                if (!returnObj.mats.length) return returnObj;
+                returnObj.searchFor = returnObj.mats.map(m => m.rar_id.toString());
+                returnObj.found = true;
+                return returnObj;
+            };
+
+            const isUniqueEntryMatch = (mat, u) => mat.mattype === u;
+            const isMatEntryMatch = (mat, r) => mat.rar_id === convertRarToID(r);
+            
+            const updatedEntryList = [];
+            for (const key of materials.keys()){
+                const matStoreObj = await grabStoredMaterials(key);
+                if (!matStoreObj.found) continue;
+
+                const matDataObj = JSON.parse((await grabMatTypeEntry(key)).matdata);
+
+                /**
+                 * This function handles first checking if the given `r` is parsable to num
+                 * 
+                 * IF NOT: uses `isUniqueEntryMatch` as the `.find()` predicate
+                 * 
+                 * IF IS: uses `isMatEntryMatch` as the `.find()` predicate
+                 * 
+                 * Both are used to `.find()` a match from the `matStoreObj.mats[]` array.
+                 * @param {string} r Either rar_id string, or `UniqueMatch` string
+                 * @returns {number} Owned amount of material match found
+                 */
+                const grabStoredAmount = r => {
+                    const matchesTypeCheck = (isNaN(r)) ? isUniqueEntryMatch : isMatEntryMatch;
+                    const matchingMat = matStoreObj.mats.find(mat => matchesTypeCheck(mat, r));
+                    return matchingMat.amount;
+                }
+
+                /**
+                 * @type {{ [r:string]: number }}
+                 */
+                const updatedMatDataObj = Object.entries(matDataObj)
+                .reduce((acc, [k, v]) => {
+                    // Is Match Found?
+                    if (!matStoreObj.searchFor.includes(k)){
+                        acc[k] = v; // NO
+                    } else acc[k] = grabStoredAmount(k); // YES
+                    return acc;
+                }, {});
+
+                const updatedMatEntry = await updateMatTypeEntry(key, updatedMatDataObj);
+                updatedEntryList.push(updatedMatEntry);
+            }
+
+            const showDetails = (ele, i) => {
+                console.log(`UserMaterials Table #${i}:\n`, ele.dataValues);
+            };
+
+            updatedEntryList.forEach(showDetails);
         }
         
-        
+        /**
+         * This function attempts to create a table for each material type stored within `client.materials.keys()`.
+         * If a table for the given user and `key` is found do nothing, otherwise create a JSON object from the stored
+         * material refs returned with `materials.get(key)`. 
+         * @param {object} interaction Base Interaction Object
+         */
         async function generateMaterialStores(interaction){
             const {materials} = interaction.client;
         
